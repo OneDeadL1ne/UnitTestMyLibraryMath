edf7520349b43455d2107f15f2e11cd40f89a0cf
DeadLine <mikemp201@gmail.com>
2023-06-05 16:22:28 +0300

    Исправил консольное приложение

--- ConLibrary/Program.cs
+++ ConLibrary/Program.cs
@@ -11,19 +11,46 @@ namespace ConLibrary
     {
         static void Main(string[] args)
         {
+            //Алгебра
+            Console.WriteLine($"Корни квадратного ур-я  {AlgebraClass.GetRootsTheQuadraticEquation(2, -5, 3).GetRoots}\n");
 
+            Console.WriteLine($"Корень линейного ур-я {AlgebraClass.GetRootTheLinearEquation(8, -4).GetRoot}\n");
 
+            double[] array = { 5, 6, 2, 90, 1 };
+            Console.Write($"Последовательность ");
+            foreach( var item in array )
+            {
+                Console.Write($"{item}");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine($"Сумма последовательность {AlgebraClass.GetRowSum(array)}\n");
 
+            Console.WriteLine($"Максимальное значение последовательности {AlgebraClass.GetMaxFromRow(array).GetValue} \n");
 
+            Console.WriteLine($"Среднее значение последовательности {AlgebraClass.GetRowAvg(array)} \n");
 
-            //Console.WriteLine(AlgebraClass.GetRootsTheQuadraticEquation(2,-5,3).x1);
-            //Console.WriteLine(AlgebraClass.GetRootsTheQuadraticEquation(2,-5,3).x2);
-            Console.WriteLine(AlgebraClass.GetRootsTheQuadraticEquation(2,-5,3).GetRoots.x1);
-            //Console.WriteLine(AlgebraClass.GetRootsTheQuadraticEquation(1,-6,9).CountRoots);
+            //Геометрия
+            Console.WriteLine($"Площадь треугольника {GeometryClass.GetSquareTriangle(10, 10, 10)} \n");
 
-            //Console.WriteLine(AlgebraClass.GetRootTheLinearEquation(0,0).x);
+            Console.WriteLine($"Площадь прямоугольного треугольника {GeometryClass.GetSquareRectangularTriangle(3, 5)} \n");
 
+            Console.WriteLine($"Площадь Прямоугольника {GeometryClass.GetSquareRectangle(5,4)} \n");
 
+            Console.WriteLine($"Площадь Квадрата {GeometryClass.GetSquareQuadrate(5)} \n");
+
+            Console.WriteLine($"Площадь Круга {GeometryClass.GetSquareCircle(2)} \n");
+
+            Console.WriteLine($"Площадь трапеции {GeometryClass.GetSquareTrapezoid(2,5,3)} \n");
+
+            //Тригонометрия
+            int angle = 60;
+            Console.WriteLine($"Синус 60 = {TrigonometryClass.GetSinAngle(angle)} \n");
+
+            Console.WriteLine($"Косинус 60 = {TrigonometryClass.GetCosAngle(angle)} \n");
+
+            Console.WriteLine($"Тангенс 60 = {TrigonometryClass.GetTgAngle(angle)} \n");
+
+            Console.WriteLine($"Котангенс 60 = {TrigonometryClass.GetCtgAngle(angle)} \n");
 
             Console.ReadKey();
             
